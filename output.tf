014233708a35eea5de7cf2f044e47edf32f85dc7
Michał Lemke <michal.lemke@netguru.pl>
2018-09-06 09:14:31 +0200

    add type as output

--- output.tf
+++ output.tf
@@ -6,6 +6,10 @@ output "name" {
   value = "${hcloud_server.server.name}"
 }
 
+output "type" {
+  value = "${hcloud_server.server.type}"
+}
+
 output "status" {
   value = "${hcloud_server.server.status}"
 }
